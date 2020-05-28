779820a6de069f3c4776bb59af90e72e0d4083a5
Lv Tongsheng <lvtongsheng@126.com>
2020-05-28 19:29:14 +0800

    push to github

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:FirstFlutterProject/random_words.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: '名字生成器',
+      home: RandomWords(),
+    );
+  }
+}
